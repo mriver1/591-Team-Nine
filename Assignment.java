92a3590a070388373a456246b82281bc3c16de29
mriver1 <36418396+mriver1@users.noreply.github.com>
2018-11-27 21:19:32 -0500

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,25 @@
+
+
+public class Assignment {
+
+	private String name;
+	private Float weight;
+	private Float grade; 
+
+	
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public Float getWeight() {
+		return weight; 
+	
+	}
+	
+	public Float getGrade() {
+		return grade; 
+	}
+
+	
+}
